88fe118886cbf84d5f636e26d6600126a92d24f2
Rajan Surani <rajansurani14@gmail.com>
2022-08-02 10:33:58 +0530

    added support for screenshare with audio

--- src/meetingContainer/mainViewContainer/PresenterView.js
+++ src/meetingContainer/mainViewContainer/PresenterView.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { ScreenShare } from "@material-ui/icons";
 import { useMeeting, useParticipant } from "@videosdk.live/react-sdk";
 import { Box, Button, Typography, useTheme } from "@material-ui/core";
@@ -26,6 +26,7 @@ const PresenterView = ({ presenterId }) => {
     micOn,
     isLocal,
     screenShareStream,
+    screenShareAudioStream,
     screenShareOn,
     displayName,
     pin,
@@ -89,6 +90,32 @@ const PresenterView = ({ presenterId }) => {
     meetingLayout,
   ]);
 
+  const audioPlayer = useRef();
+
+  useEffect(() => {
+    if (
+      !isLocal &&
+      audioPlayer.current &&
+      screenShareOn &&
+      screenShareAudioStream
+    ) {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(screenShareAudioStream.track);
+
+      audioPlayer.current.srcObject = mediaStream;
+      audioPlayer.current.play().catch((err) => {
+        if (
+          err.message ===
+          "play() failed because the user didn't interact with the document first. https://goo.gl/xX8pDD"
+        ) {
+          console.error("audio" + err.message);
+        }
+      });
+    } else {
+      audioPlayer.current.srcObject = null;
+    }
+  }, [screenShareAudioStream, screenShareOn, isLocal]);
+
   return (
     <div
       onMouseEnter={() => {
@@ -122,6 +149,8 @@ const PresenterView = ({ presenterId }) => {
             : "flex",
       }}
     >
+      <audio autoPlay playsInline controls={false} ref={audioPlayer} />
+
       <div
         style={{
           height: mobilePortrait ? "50%" : "100%",
