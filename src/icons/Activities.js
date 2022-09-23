d03a6dcd1089b1155f5078b99e3d06dee4bd01f5
BhumiSalat <bhumisalat98@gmail.com>
2022-09-23 14:53:23 +0530

    Activity icon change

--- src/icons/Activities.js
+++ src/icons/Activities.js
@@ -1,33 +1,24 @@
-import * as React from "react";
+import { SvgIcon } from "@material-ui/core";
+import React from "react";
 
-const Activities = (props) => (
-  <svg
-    width={24}
-    height={24}
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <mask
-      id="a"
-      style={{
-        maskType: "alpha",
-      }}
-      maskUnits="userSpaceOnUse"
-      x={0}
-      y={0}
-      width={24}
-      height={24}
-    >
-      <path fill="#D9D9D9" d="M0 0h24v24H0z" />
-    </mask>
-    <g mask="url(#a)">
-      <path
-        d="M11.167 11.81 2 7.957v8.589a1.878 1.878 0 0 0 1.111 1.689L11.1 22h.067V11.81ZM12 10.334l9.456-3.967a1.8 1.8 0 0 0-.567-.422l-8-3.744a2.078 2.078 0 0 0-1.778 0l-8 3.744a1.8 1.8 0 0 0-.567.422L12 10.333Zm.833 1.478V22h.056l8-3.767A1.878 1.878 0 0 0 22 16.556v-8.6l-9.167 3.855Z"
-        fill={props.fillColor}
-      />
-    </g>
-  </svg>
-);
+const Activities = (props) => {
+  return (
+    <SvgIcon {...props} viewBox="0 0 20 20">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+      >
+        <path
+          id="Path_6057"
+          data-name="Path 6057"
+          d="M12.167,12.811,3,8.956v8.589a1.878,1.878,0,0,0,1.111,1.689L12.1,23h.067ZM13,11.333l9.456-3.967a1.8,1.8,0,0,0-.567-.422l-8-3.744a2.078,2.078,0,0,0-1.778,0l-8,3.744a1.8,1.8,0,0,0-.567.422Zm.833,1.478V23h.056l8-3.767A1.878,1.878,0,0,0,23,17.556v-8.6Z"
+          transform="translate(-3 -3)"
+        />
+      </svg>
+    </SvgIcon>
+  );
+};
 
 export default Activities;
